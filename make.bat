df8656a6aabf83a52b77ce3b243cae80a1f43a2a
webwarrior <reg@webwarrior.ws>
2024-09-24 13:08:03 +0200

    build: proper make.bat for Windows
    Modify make.bat to check for cargo and build GUI. Only GUI part
    can be built on Windows.

--- make.bat
+++ make.bat
@@ -1,1 +1,6 @@
-cargo build
+@echo off
+where cargo > nul 2>&1 || (echo "Please install Rust 1.70 (or newer) toolchain; e.g. using rustup" && exit 1)
+@echo on
+
+cd gui
+cargo build --release --no-default-features
